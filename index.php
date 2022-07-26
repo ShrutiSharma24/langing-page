d94ad34cdad33f637e0e1fd7092938fbe6407659
ShrutiSharma24 <107642901+ShrutiSharma24@users.noreply.github.com>
2022-07-26 22:46:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="lcss/style.css">
+  </head>
+
+  <body>
+    <section class="hero-section">
+
+      <nav class="navbar">
+        <h2>classics bookstore</h2>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Genres</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+      <div class="hero-text">
+        <h2>Destination for every reader</h2>
+        <p>In a good book the best is between the lines!!</p>
+        <a href="#">Order Now</a>
+      </div>
+    </section>
+  </body>
+
+</html>
